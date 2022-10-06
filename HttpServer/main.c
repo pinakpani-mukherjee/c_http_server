71fa9f67ee2642e3e8a1f87ae8884f291a9cb2e3
Pinakpani Mukherjee <mukherjee.pinakpani14@gmail.com>
2022-10-06 22:21:17 +0530

    Initial Commit

---
+++ HttpServer/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  HttpServer
+//
+//  Created by Pinakpani Mukherjee on 2022/10/06.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
